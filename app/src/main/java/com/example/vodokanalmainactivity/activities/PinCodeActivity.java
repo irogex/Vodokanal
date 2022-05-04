9520fa3770d3269a98e7df79f8b8c819c8e63b7c
Alex <irogex.studio@gmail.com>
2022-05-04 12:49:41 +0800

    Обновили Пин Код Активити (фон, кнопки, убрали выход - также как и на Валид)

--- app/src/main/java/com/example/vodokanalmainactivity/activities/PinCodeActivity.java
+++ app/src/main/java/com/example/vodokanalmainactivity/activities/PinCodeActivity.java
@@ -1,7 +1,5 @@
 package com.example.vodokanalmainactivity.activities;
 
-import static android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,8 +19,12 @@ public class PinCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin_code);
-        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.white));
-        getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        //getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.white));
+       // getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.light_none));
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         dataPreferences = ((App) getApplication()).dataPreferences;
     }
    public void onEditCode(View button){
