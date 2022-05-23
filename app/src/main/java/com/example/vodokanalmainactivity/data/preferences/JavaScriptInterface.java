c7ebd6af1c7ac6c41920178df9b51df3d1e01449
Alex <irogex.studio@gmail.com>
2022-05-23 13:11:39 +0800

    refactoring

--- app/src/main/java/com/example/vodokanalmainactivity/data/preferences/JavaScriptInterface.java
+++ app/src/main/java/com/example/vodokanalmainactivity/data/preferences/JavaScriptInterface.java
@@ -1,25 +1,15 @@
 package com.example.vodokanalmainactivity.data.preferences;
 
-import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
-
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import com.example.vodokanalmainactivity.App;
 import com.example.vodokanalmainactivity.activities.WebViewActivity;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import com.example.vodokanalmainactivity.domain.OpenPdf;
 
 public class JavaScriptInterface extends AppCompatActivity {
 
@@ -30,7 +20,10 @@ public class JavaScriptInterface extends AppCompatActivity {
         mContext = c;
         dataPreferences = ((App) c.getApplication()).dataPreferences;
     }
-   // @JavascriptInterface
+
+
+
+    // @JavascriptInterface
   //  public void onCreate(Bundle savedInstanceState) {
   //      Toast.makeText(mContext,"asd", Toast.LENGTH_SHORT).show();
    //     super.onCreate(savedInstanceState);
@@ -69,95 +62,96 @@ public class JavaScriptInterface extends AppCompatActivity {
     }
 
     @JavascriptInterface
-    public void showPdf(byte[] toast)  {
+    public void showPdf(byte[] bytes)  {
       //  Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
 //        Log.d("LOGLOG", Arrays.toString(toast));
-        openPdf(toast);
+//        openPdf(toast);
+        (new OpenPdf()).execute(mContext,bytes);
 //        Intent i = new Intent( mContext, PdfActivity.class);
 //        i.putExtra("pdfsend", toast);
 //        mContext.startActivity(i);
         //Скормить классу PDF
     }
-    private void openPdf(byte[] toast){
-
-
-        try {
-            File dir = new File(mContext.getFilesDir(), "mydir");
-            if (!dir.exists()) {
-                dir.mkdir();
-            }
-            File file = new File(dir.getPath() + "/" + "FileName1.pdf");
-
-
-            file.createNewFile();
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                Files.copy(
-                        new ByteArrayInputStream(toast),
-                        file.toPath(),
-                        StandardCopyOption.REPLACE_EXISTING);
-            }
-            Uri uri = FileProvider.getUriForFile(
-                    mContext, mContext.getPackageName() + ".provider",
-                    file
-            );
-            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-            intent.setDataAndType(uri, "application/pdf");
-            intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(FLAG_GRANT_READ_URI_PERMISSION);
-            mContext.startActivity(intent);
-
-
-// 1           string contentDisposition;
-//            if (Request.Browser.Browser == "IE" && (Request.Browser.Version == "7.0" || Request.Browser.Version == "8.0"))
-//                contentDisposition = "attachment; filename=" + Uri.EscapeDataString(fileName);
-//            else if (Request.Browser.Browser == "Safari")
-//                contentDisposition = "attachment; filename=" + fileName;
-//            else
-//                contentDisposition = "attachment; filename*=UTF-8''" + Uri.EscapeDataString(fileName);
-// 1           Response.AddHeader("Content-Disposition", contentDisposition);
-
+//    private void openPdf(byte[] toast){
 //
-//            //file.write(PDF.getBytes());
-//            // file.close();
-//            Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
 //
-//            // PDFView pdfView = findViewById(R.id.pdfView);
-//            PDFView pdfView = findViewById(R.id.pdfView);
-//            //   pdfView.fromBytes(PDF.getBytes());
-//            //  pdfView.fromAsset(dir.getPath()+ "/"+"FileName1.pdf");
-////        if(pdfView==null){
-////            Log.d("fregfaer","vreve");
-////        }
-////        Log.d("fregfaer",PDF);
+//        try {
+//            File dir = new File(mContext.getFilesDir(), "mydir");
+//            if (!dir.exists()) {
+//                dir.mkdir();
+//            }
+//            File file = new File(dir.getPath() + "/" + "FileName1.pdf");
 //
-////            pdfView.fromFile(file)
-//                       pdfView.fromAsset("Prints2.pdf")
-//                    .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
-//                    .enableSwipe(true) // allows to block changing pages using swipe
-//                    .swipeHorizontal(false)
-//                    .enableDoubletap(true)
-//                    .defaultPage(0)
-//                    .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
-//                    .password(null)
-//                    .scrollHandle(null)
-//                    .enableAntialiasing(true) // improve rendering a little bit on low-res screens
-//                    // spacing between pages in dp. To define spacing color, set view background
-//                    .spacing(0)
-//                    .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
-//                    .load();
-////        } catch (FileNotFoundException e) {
-////            e.printStackTrace();
-////        } catch (IOException e) {
-////            e.printStackTrace();
-////        }
 //
-//            //2
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e("LOGLOG", e.toString());
-        }
-    }
+//            file.createNewFile();
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                Files.copy(
+//                        new ByteArrayInputStream(toast),
+//                        file.toPath(),
+//                        StandardCopyOption.REPLACE_EXISTING);
+//            }
+//            Uri uri = FileProvider.getUriForFile(
+//                    mContext, mContext.getPackageName() + ".provider",
+//                    file
+//            );
+//            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+//            intent.setDataAndType(uri, "application/pdf");
+//            intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//            intent.addFlags(FLAG_GRANT_READ_URI_PERMISSION);
+//            mContext.startActivity(intent);
+//
+//
+//// 1           string contentDisposition;
+////            if (Request.Browser.Browser == "IE" && (Request.Browser.Version == "7.0" || Request.Browser.Version == "8.0"))
+////                contentDisposition = "attachment; filename=" + Uri.EscapeDataString(fileName);
+////            else if (Request.Browser.Browser == "Safari")
+////                contentDisposition = "attachment; filename=" + fileName;
+////            else
+////                contentDisposition = "attachment; filename*=UTF-8''" + Uri.EscapeDataString(fileName);
+//// 1           Response.AddHeader("Content-Disposition", contentDisposition);
+//
+////
+////            //file.write(PDF.getBytes());
+////            // file.close();
+////            Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
+////
+////            // PDFView pdfView = findViewById(R.id.pdfView);
+////            PDFView pdfView = findViewById(R.id.pdfView);
+////            //   pdfView.fromBytes(PDF.getBytes());
+////            //  pdfView.fromAsset(dir.getPath()+ "/"+"FileName1.pdf");
+//////        if(pdfView==null){
+//////            Log.d("fregfaer","vreve");
+//////        }
+//////        Log.d("fregfaer",PDF);
+////
+//////            pdfView.fromFile(file)
+////                       pdfView.fromAsset("Prints2.pdf")
+////                    .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+////                    .enableSwipe(true) // allows to block changing pages using swipe
+////                    .swipeHorizontal(false)
+////                    .enableDoubletap(true)
+////                    .defaultPage(0)
+////                    .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+////                    .password(null)
+////                    .scrollHandle(null)
+////                    .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+////                    // spacing between pages in dp. To define spacing color, set view background
+////                    .spacing(0)
+////                    .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+////                    .load();
+//////        } catch (FileNotFoundException e) {
+//////            e.printStackTrace();
+//////        } catch (IOException e) {
+//////            e.printStackTrace();
+//////        }
+////
+////            //2
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            Log.e("LOGLOG", e.toString());
+//        }
+//    }
     @JavascriptInterface
     public void processHTML(String login, String pass)
     {
